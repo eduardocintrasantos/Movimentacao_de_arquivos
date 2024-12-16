0e6c67c8582b3a5eca5028259ed97189f11c9648
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2024-12-16 11:29:02 -0300

    16/12/2024

--- projeto.py
+++ projeto.py
@@ -19,10 +19,33 @@ arquivos = os.listdir(diretorio_origem)
 info_arquivos = {}
 
 # Opção usando for para consultar os arquivos que contem '_processado' na descrição
-for arquivos in os.listdir(diretorio_origem):
-    if '_processado' in arquivos:
-        print(arquivos)
+# for arquivos in os.listdir(diretorio_origem):
+#    if '_processado' in arquivos:
+#        print(arquivos)
 
 # Conulsar os arquivos que contem '_processado' utilizando list comprehension
 arquivos_processados = [arquivo for arquivo in arquivos if arquivo.endswith('_processado.xlsx')]
 print(arquivos_processados)
+
+# Pegar cada arquivo processado e mover para a pasta 'relatorios processados'
+for arquivo_processado in arquivos_processados:
+    caminho_completo_origem = os.path.join(diretorio_origem, arquivo_processado)
+    caminho_completo_destino = os.path.join(diretorio_destino, arquivo_processado)
+
+    shutil.move(caminho_completo_origem, caminho_completo_destino)
+
+    print(f'Arquivo {caminho_completo_origem} movido para {caminho_completo_destino}')
+
+# Adicionando informações adicionais sobre os arquivos movidos
+for arquivo in arquivos_processados:
+    caminho_completo = os.path.join(diretorio_destino, arquivo)
+
+    # Salvando as informações de tamanho e data criação dos arquivos movidos 
+    tamanho = os.path.getsize(caminho_completo)
+    data_criacao = time.ctime(os.path.getctime(caminho_completo))
+
+    # Adicionando as informações ao dicionario criado
+    info_arquivos[arquivo] = [tamanho, data_criacao]
+
+print(info_arquivos)
+print(f'Quantidade de arquivos movidos: {len(info_arquivos.keys())}')
